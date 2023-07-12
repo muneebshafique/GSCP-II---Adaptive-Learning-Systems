5b14bf268acb614fe139d8ccbaa1093990123c41
Laiba Qureshi <lq06668@st.habib.edu.pk>
2023-07-13 00:52:16 +0500

    Update student_model.py

--- Code/Modules/student_model.py
+++ Code/Modules/student_model.py
@@ -1,6 +1,7 @@
 import random
 import copy
 import knowledge_base
+import math
 
 MAX_PROFICIENCY = 10
 SIG_FIGURES=3
@@ -12,7 +13,8 @@ class StudentModel:
      
         self.topic_proficiency={'Motion, forces and energy': {'Physical quantities and measurement techniques': 0.566, 'Motion': 0.628, 'Mass and Weight': 0.544, 'Density': 0.962, 'Forces': 0.528, 'Momentum': 0.446, 'Energy, work and power ': 0.616, 'Pressure': 0.294}, 'Thermal physics': {'Kinetic particle model of matter ': 0.469, 'Thermal properties and temperature': 0.35, 'Transfer of thermal energy ': 0.354}, 'Waves': {'General properties of waves ': 0.936, 'Light ': 0.913, 'Electromagnetic spectrum': 0.051, 'Sound ': 0.889}, 'Electricity and magnetism': {'Simple magnetism and magnetic field ': 0.974, 'Electrical quantities ': 0.473, 'Electric Circuits': 0.541, 'Practical Electricity': 0.463, 'Electromagnetic effects ': 0.757, 'Uses of Oscilloscope': 0.551}, 'Nuclear physics': {'The nuclear model of the atom ': 0.449, 'Radioactivity ': 0.241}, 'Space physics': {'Earth and the Solar System ': 0.477, 'Stars and the Universe ': 0.233}}
         self.subtopic_proficiency = {'Forces': {'Balanced and unbalanced forces': 0.344, 'Friction': 0.43, 'Elastic deformation': 0.905, 'Circular motion': 0.201, 'Turning effect of forces': 0.492, 'Centre of gravity': 0.796}, 'Energy, work and power ': {'Energy ': 0.525, 'Work': 0.853, 'Energy resources': 0.871, 'Efficiency': 0.642, 'Power': 0.187}, 'Kinetic particle model of matter ': {'States of matter ': 0.208, 'Particle model ': 0.731}, 'Thermal properties and temperature': {'Thermal expansion of solids, liquids and gases ': 0.022, 'Specific heat capacity ': 0.538, 'Melting, boiling and evaporation ': 0.491}, 'Transfer of thermal energy ': {'Conduction ': 0.88, 'Convection ': 0.019, 'Radiation ': 0.464, 'Consequences of thermal energy transfer ': 0.052}, 'Light ': {'Reflection of light ': 0.915, 'Refraction of light ': 0.845, 'Thin lenses ': 0.908, 'Dispersion of light ': 0.985}, 'Electrical quantities ': {'Electrical charge ': 0.391, 'Electrical current ': 0.157, 'Electromotive force and potential difference ': 0.735, 'Resistance ': 0.611}, 'Electric Circuits': {'Circuit diagram and circuit components': 0.714, 'Series and parallel circuits': 0.665, 'Action and use of circuit components': 0.243}, 'Practical Electricity': {'Uses of electricity': 0.109, 'Electrical Safety': 0.818}, 'Electromagnetic effects ': {'Electromagnetic induction ': 0.856, 'The a.c. generator ': 0.956, 'Magnetic effect of a current ': 0.942, 'Forces on a current-carrying conductor ': 0.7, 'The d.c. motor ': 0.564, 'The transformer ': 0.527}, 'The nuclear model of the atom ': {'The atom ': 0.746, 'The nucleus ': 0.151}, 'Radioactivity ': {'Detection of radioactivity ': 0.246, 'The three types of emission ': 0.443, 'Radioactive decay ': 0.244, 'Fission and fusion ': 0.036, 'Half-life ': 0.013, 'Safety precautions ': 0.464}, 'Earth and the Solar System ': {'The earth ': 0.852, 'The solar system ': 0.103}, 'Stars and the Universe ': {'The sun as a star ': 0.358, 'Stars ': 0.221, 'The universe ': 0.12}}
-
+        self.options = 4
+        self.K = 0.04
 
     # assigns proficiency for every topic and subtopic (0-1)
     def dummy_data_student_proficiency(self):
@@ -129,6 +131,48 @@ class StudentModel:
         self.topic_proficiency=self.update_topic_proficiencies(self.topic_proficiency, self.subtopic_proficiency)
         print("\n-----------POST-UPDATE TOPIC PROFICIENCY----------")
         print(self.topic_proficiency)
+
+    def Elo_update_student_proficiency(self, paper, response, topic_section_mapping):
+        self.difficulty = {1: 0.33, 2: 0.67, 3: 0.99}
+        for i in range(len(response)):
+            qs_info = paper[i+1]
+            for topic_info in qs_info:
+                topic, sub_topic, difficulty = topic_info[0], topic_info[1], topic_info[2]
+                difficulty_in_dec = self.difficulty[difficulty]
+                if sub_topic == 'None':
+                    section = topic_section_mapping[topic]
+                    skill = self.topic_proficiency[section][topic]
+                    student_attempt = response[i]
+                    correct_prob = self.Logistic_Function_2(
+                        difficulty_in_dec, skill)
+                    updated_skill = self.change_in_skill(
+                        student_attempt, correct_prob, skill)
+                    self.topic_proficiency[section][topic] = updated_skill
+                else:
+                    skill = self.subtopic_proficiency[topic][sub_topic]
+                    student_attempt = response[i]
+                    correct_prob = self.Logistic_Function_2(
+                        difficulty_in_dec, skill)
+                    updated_skill = self.change_in_skill(
+                        student_attempt, correct_prob, skill)
+                    self.subtopic_proficiency[topic][sub_topic] = updated_skill
+        self.topic_proficiency = self.update_topic_proficiencies(
+            self.topic_proficiency, self.subtopic_proficiency)
+        print("\n-----------POST-UPDATE TOPIC PROFICIENCY----------")
+        print(self.topic_proficiency)
+
+    def Logistic_Function_2(self, difficulty, skill):
+        # For question with k options the probability of a correct answer becomes:
+        # P(correctsi = 1) = 1/k + (1 − 1/k)/(1 + e−(θs−di))
+        reciprocal = 1/self.options
+        denominator = 1+math.exp(-skill + difficulty)
+        correct_probability = reciprocal + ((1-reciprocal)/denominator)
+        return correct_probability
+
+    def change_in_skill(self, student_attempt, correct_prob, skill):
+        estimate = student_attempt - correct_prob
+        skill = skill + self.K*(estimate)
+        return skill
       
 
 
