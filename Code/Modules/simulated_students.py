5f1440597997888e8786fa4d70acbe8b16698b86
Laiba Qureshi <lq06668@st.habib.edu.pk>
2023-08-07 22:57:00 +0500

    Update simulated_students.py

--- Code/Modules/simulated_students.py
+++ Code/Modules/simulated_students.py
@@ -2,16 +2,22 @@ from student_model import StudentModel
 from knowledge_base import KnowledgeBase
 from evaluation import Evaluation
 from paper_generator import PaperGenerator
+from student_initial_testing import Student_onboarding
 import random
 
 
 class Good_Simulated_Student:
     def __init__(self):
         # change your path here.
-        # filename = "C:/Users/LAIBA/Downloads/GSCP-II---Adaptive-Learning-Systems-main (1)/GSCP-II---Adaptive-Learning-Systems-main/Code/Database/Olevels Physics Data (2023-2025).csv"
-        filename ="../Database/Olevels Physics Data (2023-2025).csv"
-        self.student_model = StudentModel()
+        filename = "C:/Users/LAIBA/Desktop/GSCP-II---Adaptive-Learning-Systems-main/Code/Database/Olevels Physics Data (2023-2025).csv"
+
         self.knowledge_base = KnowledgeBase()
+        self.knowledge_base.initialize_syllabus(filename)
+        self.student_initial = Student_onboarding(self.knowledge_base.syllabus)
+        self.student_initial.generate_initial_prof()
+
+        self.student_model = StudentModel(
+            self.student_initial.topic_proficiency, self.student_initial.subtopic_proficiency)
 
         self.knowledge_base.initialize_syllabus(filename)
         self.paper_generator = PaperGenerator(
@@ -31,21 +37,18 @@ class Good_Simulated_Student:
         return response
 
     def run(self):
-        paper = self.paper_generator.generate_paper()
-        self.paper_generator.print_paper(paper)
+        paper, paper_info = self.paper_generator.generate_paper()
+        # self.paper_generator.print_paper(paper)
+        self.paper_generator.print_paper_info(paper_info)
 
-        response = self.generate_response(paper)
-        checked_paper = self.evaluate.check_paper(response, paper)
+        response = self.generate_response(paper_info)
+        checked_paper = self.evaluate.check_paper(response, paper_info)
 
-        # based on evalutation student's topic and sub-topic proficiencies are updated.
-        current_student_ability = self.student_model.Q_generate_new_proficiencies(
-            checked_paper, paper)
-        # self.student_model.Q_update_student_proficiency(
-        #     current_student_ability, self.knowledge_base.topic_section_mapping)
         self.student_model.Elo_update_student_proficiency(
-            paper, checked_paper, self.knowledge_base.topic_section_mapping)
+            paper_info, checked_paper, self.knowledge_base.topic_section_mapping)
 
 # Generate a paper in which the student performs correctly:
+
 
 program = Good_Simulated_Student()
 program.run()
@@ -54,9 +57,15 @@ program.run()
 class Bad_Simulated_Student:
     def __init__(self):
         # change your path here.
-        filename = "C:/Users/LAIBA/Downloads/GSCP-II---Adaptive-Learning-Systems-main (1)/GSCP-II---Adaptive-Learning-Systems-main/Code/Database/Olevels Physics Data (2023-2025).csv"
-        self.student_model = StudentModel()
+        filename = "C:/Users/LAIBA/Desktop/GSCP-II---Adaptive-Learning-Systems-main/Code/Database/Olevels Physics Data (2023-2025).csv"
+
         self.knowledge_base = KnowledgeBase()
+        self.knowledge_base.initialize_syllabus(filename)
+        self.student_initial = Student_onboarding(self.knowledge_base.syllabus)
+        self.student_initial.generate_initial_prof()
+
+        self.student_model = StudentModel(
+            self.student_initial.topic_proficiency, self.student_initial.subtopic_proficiency)
 
         self.knowledge_base.initialize_syllabus(filename)
         self.paper_generator = PaperGenerator(
@@ -80,19 +89,15 @@ class Bad_Simulated_Student:
         return response
 
     def run(self):
-        paper = self.paper_generator.generate_paper()
-        self.paper_generator.print_paper(paper)
+        paper, paper_info = self.paper_generator.generate_paper()
+        # self.paper_generator.print_paper(paper)
+        self.paper_generator.print_paper_info(paper_info)
 
-        response = self.generate_response(paper)
-        checked_paper = self.evaluate.check_paper(response, paper)
+        response = self.generate_response(paper_info)
+        checked_paper = self.evaluate.check_paper(response, paper_info)
 
-        # based on evalutation student's topic and sub-topic proficiencies are updated.
-        current_student_ability = self.student_model.Q_generate_new_proficiencies(
-            checked_paper, paper)
-        # self.student_model.Q_update_student_proficiency(
-        #     current_student_ability, self.knowledge_base.topic_section_mapping)
         self.student_model.Elo_update_student_proficiency(
-            paper, checked_paper, self.knowledge_base.topic_section_mapping)
+            paper_info, checked_paper, self.knowledge_base.topic_section_mapping)
 
 
 # Generate a paper in which the student performs poorly:
